bd09ce198a8e185413ff8313a94293b78a5925d5
dingamy <amyding04@gmail.com>
2024-06-14 11:43:41 -0500

    can display some latex content using matplotlib but buggy

--- main.py
+++ main.py
@@ -4,16 +4,15 @@ from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QLa
 from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtGui import QAction, QPixmap, QPalette, QColor
 import numpy as np
-
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib import pyplot as plt
 from pylatex import Document, Section, Subsection, Command, Tabular, LongTable, PageStyle, Head, LargeText, MediumText, LineBreak, MiniPage, MultiColumn, Tabularx, HugeText
 from pylatex.utils import italic, NoEscape, bold
 import os
+import io
 
-from PyQt6.QtWidgets import (
-    QMainWindow, QApplication,
-    QLabel, QCheckBox, QComboBox, QListWidget, QLineEdit,
-    QLineEdit, QSpinBox, QDoubleSpinBox, QSlider, QMessageBox
-)
+from PyQt6.QtWidgets import QMainWindow, QApplication, QLabel, QCheckBox, QComboBox, QListWidget, QLineEdit, QLineEdit, QSpinBox, QDoubleSpinBox, QSlider, QMessageBox
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -31,7 +30,7 @@ class MainWindow(QMainWindow):
         self.bussim_result = []
         self.branch_table = []
         self.season = "";
-        layout = QGridLayout()
+        self.layout = QGridLayout()
 
         self.scenario_cb = QComboBox()
         self.add_data_to_combobox("database.db", self.scenario_cb, "`Scenario Name`", "Scenarios")
@@ -39,21 +38,21 @@ class MainWindow(QMainWindow):
         self.add_data_to_combobox("database.db", self.contingency_cb, "`Contingency Name`", "Contingency")
         report = QPushButton("Generate Report")
         report.clicked.connect(self.retrieve_data)
-        layout.addWidget(QLabel('Scenario'), 0, 0)
-        layout.addWidget(self.scenario_cb, 1, 0)
-        layout.addWidget(QLabel('Contingency'), 2, 0)
-        layout.addWidget(self.contingency_cb, 3, 0)
-        layout.addWidget(report, 4, 0)
-        self.table = QTableWidget()
-        layout.addWidget(QLabel("Report"), 0, 1)
-        layout.addWidget(self.table, 1, 1,)
+        self.layout.addWidget(QLabel('Scenario'), 0, 0)
+        self.layout.addWidget(self.scenario_cb, 1, 0)
+        self.layout.addWidget(QLabel('Contingency'), 2, 0)
+        self.layout.addWidget(self.contingency_cb, 3, 0)
+        self.layout.addWidget(report, 4, 0)
+        self.latex_label = QLabel()
+        self.layout.addWidget(QLabel("Report"), 0, 1)
+        self.layout.addWidget(self.latex_label, 1, 1,)
 
         report_button = QPushButton("Save to Computer")
         report_button.clicked.connect(self.generate_report)
-        layout.addWidget(report_button, 4, 1)
+        self.layout.addWidget(report_button, 4, 1)
 
         widget = QWidget()
-        widget.setLayout(layout)
+        widget.setLayout(self.layout)
         self.setCentralWidget(widget)
         
         
@@ -93,7 +92,7 @@ class MainWindow(QMainWindow):
             bus_result_part = cursor.fetchall()
             self.bussim_result[i] += bus_result_part[0]
         
-        print(self.bussim_result)
+        #print(self.bussim_result)
         query = f"SELECT COUNT(`Bus Number`) FROM `BUS`;"
         cursor.execute(query)
         self.num_voltage = cursor.fetchall()[0][0]
@@ -108,27 +107,21 @@ class MainWindow(QMainWindow):
             branch_result_part = cursor.fetchall()
             self.branch_table[i] += branch_result_part[0]
 
-        print(self.branch_table)
+        #print(self.branch_table)
         query = f"SELECT COUNT(`Branch Name`) FROM `Branch`;"
         cursor.execute(query)
         self.num_thermalbranch = cursor.fetchall()[0][0]
+
+        self.display_report()
        
     def generate_report(self):
-        #geometry_options = {"tmargin" : "2.54cm", "margin": "2.54cm", "includeheadfoot": True, "papersize": "letterpaper"}
         geometry_options = {"margin": "2.54cm"}
         doc = Document(geometry_options=geometry_options)
        
         doc.preamble.append(NoEscape(r'\title{Steady State Contingency Analysis Report\vspace{-3ex}}'))
         doc.preamble.append(NoEscape(r'\date{Report generated: \today\vspace{-2ex}}'))
         doc.append(NoEscape(r'\maketitle'))
-        #doc.append(NoEscape(r'\noindent'))
-       # doc.append(LargeText(bold("Steady State Contingency Analysis Report")))
-        #doc.append("\n")
-       # doc.append(MediumText("Report Generated: "))
-       # doc.append(MediumText(NoEscape(r'\today')))
-        #with doc.create(MiniPage(align='c')):
-        #doc.append('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut euismod eros. Pellentesque porta velit non commodo molestie. Sed ipsum lacus, aliquet at venenatis quis, commodo vitae arcu. Fusce arcu lacus, ultrices sed dapibus a, auctor at metus. Cras nec finibus erat, at sagittis est. Vivamus sagittis elit nec lectus egestas molestie. Phasellus et venenatis turpis. Morbi hendrerit interdum urna, sed hendrerit libero ultrices at. Duis feugiat sodales eleifend. Aliquam sit amet congue quam. Nam et nulla id diam consequat scelerisque. Nullam pulvinar quam id nibh molestie, quis feugiat arcu tempor. Aliquam rhoncus suscipit ultricies.')
-        
+
         with doc.create(Section(f"Contingency: {self.contingency} (Islands created: ?)", False)):
 
             with doc.create(Subsection(f"Total number of monitored buses: {self.num_voltage}", False)):
@@ -160,9 +153,66 @@ class MainWindow(QMainWindow):
                         branch_table_view.add_hline()
                 else:
                     doc.append("No violations.")
-            
-        doc.generate_pdf('report', clean_tex=False)
+
+        doc.generate_tex('tex')
+        #f = open("tex.tex", "r")
+        #print(f.read())
+        #self.latex_to_image(f.read())
+        #doc.generate_pdf('report', clean_tex=False)
         self.done_alert()
+
+    def display_report(self):
+        self.generate_report()
+        f = open("tex.tex", "r")
+        print(f.read())
+
+        '''
+        if len(self.bussim_result) != 0:
+            latex_content += r"\begin{tabular}{|c|c|c|c|c|c|}\hline" \
+                             r"Bus Number & Bus Name & Bus Base (kV) & Low Voltage Criteria (pu) & High Voltage Criteria (pu) & Bus Voltage (pu)\\ \hline"
+            for bus in self.bussim_result:
+                latex_content += f"{self.bussim_result[0]} & {self.bussim_result[2]} & {self.bussim_result[3]} & {self.bussim_result[4]} & {self.bussim_result[5]} & {self.bussim_result[1]}\\ \hline"
+            latex_content += r"\end{tabular}\\"
+        else:
+            latex_content += r"No violations.\\"
+
+        latex_content += r"Total number of monitored branches: " + str(self.num_thermalbranch) + r"\\"
+        
+        if len(self.branch_table) != 0:
+            latex_content += r"\begin{tabular}{|c|c|c|c|c|c|c|c|}\hline" \
+                             r"Branch Name & Metered End & Other End & Branch ID & Voltage Class (kV) & Rating (Amps) & Metered End Loading (Amps) & Other End Loading (Amps)\\ \hline"
+            for branch in self.branch_table:
+                season_rating = branch[8] if self.season == "Winter" else branch[7]
+                latex_content += f"{branch[0]} & {branch[3]} & {branch[4]} & {branch[5]} & {branch[6]} & {season_rating} & {branch[1]} & {branch[2]}\\ \hline"
+            latex_content += r"\end{tabular}\\"
+        else:
+            latex_content += r"No violations.\\"
+        '''
+        print(latex_content)
+        
+        # Convert LaTeX to image
+        image = self.latex_to_image(latex_content)
+        self.latex_label.setPixmap(image)
+        
+
+    def latex_to_image(self, latex_content):
+        plt.clf()
+        plt.rcParams['text.usetex'] = True
+        plt.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}'
+
+        fig, ax = plt.subplots()
+        ax.text(0.5, 0.5, f"${latex_content}$", fontsize=12)
+        ax.axis('off')
+        plt.show()
+        
+        buf = io.BytesIO()
+        plt.savefig(buf, format='png')
+        buf.seek(0)
+        image = QPixmap()
+        image.loadFromData(buf.getvalue(), "PNG")
+        buf.close()
+        return image
+        
 
     def done_alert(self):
         dialog = QMessageBox(self)
